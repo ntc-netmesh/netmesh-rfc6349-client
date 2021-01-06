31eabbc6f0a5c25fd67064121244ef1e13f9239a
robot-1 <jeanjay.quitayen@asti.dost.gov.ph>
2021-01-07 03:25:05 +0800

    add fix server

--- create_shortcut.sh
+++ create_shortcut.sh
@@ -3,5 +3,5 @@ Type=Application
 Terminal=true
 Name=rfc6349
 Icon=utilities-terminal
-Exec=gnome-terminal -- bash -c 'cd $(pwd) && python3 update.py; sleep 5';
+Exec=gnome-terminal -- bash -c 'cd $(pwd) && make update && python3 update.py; sleep 5';
 #Path=/home/event1/Desktop/6349_App/" > rfc6349.desktop
