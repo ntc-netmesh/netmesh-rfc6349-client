e9482133263024932cb3a5da870453df74381861
wang <jcvaldez199@gmail.com>
2022-04-24 19:58:43 +0800

    prelim working updater tag 3.0.2

--- netmesh_install.py
+++ netmesh_install.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import stat
+import sys
 
 import netmesh_constants
 import netmesh_utils
@@ -16,19 +17,24 @@ def run(command):
 def install_proj():
   # Run pyinstaller
   ubuntu_version = netmesh_utils.get_ubuntu_version()
-  MAIN_DIRECTORY = netmesh_utils.resource_path('')
+
+  # MAKE SURE THAT MAIN_DIRECTORY IS AT THE APP BASE
+  temp = os.getcwd().split('dist')[0]
+  MAIN_DIRECTORY = temp if temp[-1] != '/' else temp[:-1]
 
   app_name = f'netmesh-rfc-6349-app_{netmesh_constants.APP_VERSION}_u{ubuntu_version}'
-  app_location = netmesh_utils.resource_path('netmesh_rfc6349_app.py')
-  installer_command = f'pyinstaller {app_location} -n "{app_name}" -s -F --clean --add-data "templates:templates" --add-data "static:static"'
+  #app_location = netmesh_utils.resource_path('netmesh_rfc6349_app.py')
+  app_location = f'{MAIN_DIRECTORY}/netmesh_rfc6349_app.py'
+  installer_command = f'cd {MAIN_DIRECTORY} && pyinstaller {app_location} -n "{app_name}" -s -F --clean --add-data "templates:templates" --add-data "static:static"'
 
   for line in run(installer_command):
     print(line)
 
   # Create .desktop file
-  file_path = f'{os.getcwd()}/dist/{app_name}.desktop'
+  file_path = f'{MAIN_DIRECTORY}/dist/{app_name}.desktop'
   file_action = 'x'
 
+  print("app location : ", app_location)
   print(file_path)
 
   if os.path.isfile(file_path):
@@ -44,8 +50,8 @@ def install_proj():
 
   file_execution_commands = [
     *additional_commands,
-    f"cd {MAIN_DIRECTORY} && ./dist/{app_name}" # this will open the app
-#    f"cd $(dirname %k) && ./dist/{app_name}" # this will open the app
+    f'cd {MAIN_DIRECTORY}/dist && ./{app_name}' # this will open the app
+#    f"cd $(dirname %k) && ./{app_name}" # this will open the app
   ]
 
   with open(file_path, file_action) as f:
