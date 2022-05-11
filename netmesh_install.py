d192df07b539aa6343f81981be944135761aa573
Albert Francisco <albertfrancisco124@gmail.com>
2022-05-11 15:21:12 +0800

    Add delete folder command for installing/updating the app

--- netmesh_install.py
+++ netmesh_install.py
@@ -30,9 +30,8 @@ def install_proj():
   
   installer_command = f'cd {MAIN_DIRECTORY} && pyinstaller {app_location} -n "{app_name}" --clean --splash ./static/images/rfc_splash_screen.png --add-data "templates:templates" --add-data "static:static"'
 
-  # print(f"Removing existing folder '{MAIN_DIRECTORY}/dist/{app_name}'...")
-  
-  # subprocess.Popen(['sudo', 'rm', '-rf', f'{MAIN_DIRECTORY}/dist/{app_name}'], stdout=subprocess.PIPE).communicate()
+  print(f"Removing existing folder '{MAIN_DIRECTORY}/dist/{app_name}'...")
+  subprocess.Popen(['sudo', 'rm', '-rf', f'{MAIN_DIRECTORY}/dist/{app_name}'], stdout=subprocess.PIPE).communicate()
   
   # Run pyinstaller
   for line in run(installer_command):
