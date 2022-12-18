2d11a2b09952e48aa0978d74bcc8dbf5b2f92c23
wang <jcvaldez199@gmail.com>
2022-12-18 12:42:56 +0800

    init

--- netmesh_rfc6349_app/config.py
+++ netmesh_rfc6349_app/config.py
@@ -5,7 +5,7 @@ import os
 class Config:
     FLASK_DEBUG = 1
     PERMANENT_SESSION_LIFETIME = timedelta(days=100)
-    # SECRET_KEY = 'H3kur3WoOqAt3yFIOqTwXog4'
+    SECRET_KEY = 'H3kur3WoOqAt3yFIOqTwXog4'
     # SECRET_KEY = b64encode(os.urandom(24)).decode('utf-8')
     RESULTS_SERVER_API_URI = "http://202.90.159.48/api"
     APP_LATEST_GITHUB_RELEASE_URL = "https://api.github.com/repos/ntc-netmesh/netmesh-rfc6349-client/releases/latest"
